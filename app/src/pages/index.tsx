4579a64c3b71b51e95798bfbe2b7f1bfcca2e913
SameepAher <sameepaher@gmail.com>
2023-09-28 03:20:43 +0530

    Made proposal and projects page

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -17,34 +17,34 @@ const HomePage: React.FC<HomePageProps> = () => {
   return (
     <HomeLayout>
       {/* Learn more card */}
-      <div className="rounded-xl bg-orange-500/75 px-8 py-12 shadow">
-        <div className="w-2/3">
-          <h1 className="w-4/5">Get Involved in shaping the Future of the Nation</h1>
-          <p className="mt-1">
+      <div className="rounded-xl bg-gradient-to-r from-fuchsia-600 to-pink-600 px-8 py-12 mx-0 xl:mx-44 shadow">
+        <div className="">
+          <h1 className="text-white lg:text-3xl">Get Involved in shaping the Future of the Nation</h1>
+          <p className="mt-4 text-white text-md lg:text-lg font-bold">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam odit qui aliquid
             mollitia. Nostrum odit vitae eligendi perspiciatis tenetur minima repudiandae,
             laboriosam itaque vero atque autem quasi unde a nulla adipisci exercitationem.
           </p>
         </div>
-        <div className="mt-4 flex gap-4">
-          <Button variant="secondary" className="group" title="Learn more" />
-          <Button variant="secondary" className="group" title="View Proposals" />
+        <div className="mt-8 flex gap-4">
+          <Button variant="secondary" className="group hover:translate-y-[-2px] transition-all duration-200 ease-in-out" title="Learn more" />
+          <Button variant="secondary" className="group hover:translate-y-[-2px] transition-all duration-200 ease-in-out" title="View Proposals" />
         </div>
       </div>
 
       {/* Stats */}
-      <div className="mt-6 flex w-full gap-4 [&>*]:flex [&>*]:grow [&>*]:flex-col [&>*]:gap-2">
-        <Card className="">
+      <div className="mx-0 xl:mx-44 mt-6 flex gap-4 [&>*]:flex [&>*]:grow [&>*]:flex-col [&>*]:gap-2">
+        <Card className="hover:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.2)] hover:cursor-pointer hover:translate-y-[-2px] transition-all duration-200 ease-in-out">
           <p className="">Proposals</p>
           <h2>15 active proposals</h2>
           <p>3 ending today</p>
         </Card>
-        <Card className="">
+        <Card className="hover:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.2)] hover:cursor-pointer hover:translate-y-[-2px] transition-all duration-200 ease-in-out">
           <p className="">Proposals</p>
           <h2>15 active proposals</h2>
           <p>3 ending today</p>
         </Card>
-        <Card className="">
+        <Card className="hover:shadow-[0_35px_60px_-15px_rgba(0,0,0,0.2)] hover:cursor-pointer hover:translate-y-[-2px] transition-all duration-200 ease-in-out">
           <p className="">Proposals</p>
           <h2>15 active proposals</h2>
           <p>3 ending today</p>
@@ -52,7 +52,7 @@ const HomePage: React.FC<HomePageProps> = () => {
       </div>
 
       {/* Proposals */}
-      <div className="mt-12">
+      <div className="mx-0 xl:mx-44 mt-12">
         <h1>Open Proposals</h1>
         <p className="mt-2 opacity-75">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque suscipit error dicta
@@ -69,7 +69,7 @@ const HomePage: React.FC<HomePageProps> = () => {
       </div>
 
       {/* Projects */}
-      <div className="mt-12">
+      <div className="mx-0 xl:mx-44 mt-12">
         <h1>Open Projects</h1>
         <p className="mt-2 opacity-75">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque suscipit error dicta
@@ -86,7 +86,7 @@ const HomePage: React.FC<HomePageProps> = () => {
       </div>
 
       {/* Community engagement */}
-      <div className="mt-12">
+      <div className="mx-0 xl:mx-44 mt-12">
         <h1>Community Engagement</h1>
         <p className="mt-2 opacity-75">
           See the participation signal from the community and understand the basic health stats from
@@ -102,6 +102,22 @@ const HomePage: React.FC<HomePageProps> = () => {
           <div className="w-[95%] md:w-full lg:col-span-4">
             <CommunityInventory />
           </div>
+        </div>
+      </div>
+
+      {/* stats and forum links */}
+      <div className="mt-4 rounded-xl bg-gradient-to-r from-fuchsia-600 to-pink-600 px-8 py-12 mx-0 xl:mx-44 shadow">
+        <div className="">
+          <h1 className="text-white lg:text-3xl">Get Involved in shaping the Future of the Nation</h1>
+          <p className="mt-4 text-white text-md lg:text-lg font-bold">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam odit qui aliquid
+            mollitia. Nostrum odit vitae eligendi perspiciatis tenetur minima repudiandae,
+            laboriosam itaque vero atque autem quasi unde a nulla adipisci exercitationem.
+          </p>
+        </div>
+        <div className="mt-8 flex gap-4">
+          <Button variant="secondary" className="group hover:translate-y-[-2px] transition-all duration-200 ease-in-out" title="Learn more" />
+          <Button variant="secondary" className="group hover:translate-y-[-2px] transition-all duration-200 ease-in-out" title="View Proposals" />
         </div>
       </div>
     </HomeLayout>
